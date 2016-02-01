a7b1f50563dcde7b96e56fabd7770135e694e5dd
Joachim Andersen <joachimdamandersen@gmail.com>
2016-02-01 22:09:33 +0100

    added files for ex 1

---
+++ 01 - Hand testing calculator/CalculatorTestVSP/CalculatorTestVSP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTestVSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+
+            Console.WriteLine(c.Power(2, 3));
+
+            Console.WriteLine(c.Add(3, 7));
+
+            Console.WriteLine(c.Multiply(10, 21));
+
+            Console.WriteLine(c.Subtract(20, 80));
+
+        }
+    }
+}
